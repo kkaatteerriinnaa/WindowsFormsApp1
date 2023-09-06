67f3fcd8c442038c5d42ed77c9bc30bf54b17367
kkaat <kkaat@KATERINA>
2023-09-06 20:57:17 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(36, 480);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(728, 90);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(244, 28);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "График";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, -119);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(680, 593);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 593);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] Items =
+            {
+                "Линейная",
+                "Квадтратическая",
+                "Кубическая",
+                "Раицональная",
+                "Логарифмическая"
+            };
+            comboBox1.Items.AddRange(Items);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Linean_func(Graphics g)
+        {
+            int x1 = 0, x2 = 1;
+            int y1 = 3 * x1 + 0;
+            int y2 = 3 * x2 + 0;
+            g.DrawLine(new Pen(Color.Red, 2.0f), x1 * 60, y1 * (-60), x2 * 60, y2 * (-60));
+            g.DrawLine(new Pen(Color.Red, 2.0f), x1 * -60, y1 * (60), x2 * -60, y2 * (60));
+        }
+
+        public void Quadrant_func(Graphics g)
+        {
+            Pen pen = new Pen(Color.Green, 2.0f);
+
+            Point[] points =
+            {
+                new Point(-100, -200),
+                new Point(0, 0),
+                new Point(100, -200)
+            };
+            g.DrawCurve(pen, points);
+            pen.Dispose();
+        }
+
+        public void Cubic_func(Graphics g)
+        {
+            Pen pen = new Pen(Color.Blue, 2.0f);
+            double a = 1, b = 0, c = 0, d = 0;
+            List<PointF> points = new List<PointF>();
+            for (double x = -20; x <= 20; x += 0.1)
+            {
+                double y = a * Math.Pow(x, 3) + b * Math.Pow(x, 2) + c * x + d;
+                points.Add(new PointF((float)x * 20, (float)y * (-20)));
+            }
+            g.DrawLines(pen, points.ToArray());
+            pen.Dispose();
+        }
+
+        public void Rational_func(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 2.0f);
+            double a = 1, b = -2, c = 1, d = 1, e = 2;
+            for (int x = 0; x <= 200; x++)
+            {
+                double y = (a * x * x + b * x + c) / (d * x + e);
+                int yPixel = 0 - (int)y;
+                g.FillRectangle(Brushes.YellowGreen, x, yPixel, 2, 2);
+            }
+            pen.Dispose();
+        }
+
+        public void Loga(Graphics g)
+        {
+            Pen pen = new Pen(Color.LightBlue, 2.0f);
+            double a = 2;
+            List<PointF> points = new List<PointF>();
+            for (double x = 0.1; x <= 20; x += 0.1)
+            {
+                double y = Math.Log(x, a);
+                points.Add(new PointF((float)x * 20, (float)y * (-20)));
+            }
+            g.DrawLines(pen, points.ToArray());
+            pen.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            Point[] lineY = { new Point(10, -190), new Point(0, -200), new Point(-10, -190) };
+            Point[] lineX = { new Point(190, 10), new Point(200, 0), new Point(190, -10) };
+            Pen pen = new Pen(Color.Black, 2.0f);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            Matrix x = new Matrix(1, 0, 0, -1, 0, 0);
+            g.TranslateTransform(260, 260, MatrixOrder.Append);
+            g.DrawString("Y", Font, Brushes.Black, new Point(-20, -210));
+            g.DrawString("X", Font, Brushes.Black, new Point(200, -20));
+            g.DrawLine(pen, -200, 0, 200, 0);
+            g.DrawLine(pen, 0, -200, 0, 200);
+            g.DrawLines(pen, lineX);
+            g.DrawLines(pen, lineY);
+            int X = -200;
+            for (; X < 200; X += 20)
+                g.DrawLine(pen, X, -5, X, 5);
+            int Y = -180;
+            for (; Y < 200; Y += 20)
+                g.DrawLine(pen, -5, Y, 5, Y);
+            if (comboBox1.SelectedItem.ToString() == "Линейная")
+                Linean_func(g);
+            if (comboBox1.SelectedItem.ToString() == "Квадтратическая")
+                Quadrant_func(g);
+            if (comboBox1.SelectedItem.ToString() == "Кубическая")
+                Cubic_func(g);
+            if (comboBox1.SelectedItem.ToString() == "Раицональная")
+                Rational_func(g);
+            if (comboBox1.SelectedItem.ToString() == "Логарифмическая")
+                Loga(g);
+            pen.Dispose();
+            g.Dispose();
+        }
+    }
+}
